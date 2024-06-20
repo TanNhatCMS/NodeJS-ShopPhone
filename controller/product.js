813c300a903793c7ed61b412f12e05c5d95680de
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-06-20 18:59:56 +0700

    Update product.js

--- controller/product.js
+++ controller/product.js
@@ -1,44 +1,48 @@
 const Product = require("../models/Product");
 
 exports.Addproduct = (req, res, next) => {
-    console.log(req.body)
-    const products = new Product(req.body);
-    products.save();
-    res.status(201).json({
-         message: 'Created product'
-    })
-}
+    const product = new Product(req.body);
+    product.save((err) => {
+        if (err) {
+            return res.status(500).json({ message: 'Failed to create product' });
+        }
+        res.status(201).json({ message: 'Created product' });
+    });
+};
 
 exports.getProducts = (req, res) => {
-    Product.fetchData(products => {
-        res.status(200).json({
-            products: products
-         })
-    })
-}
+    Product.fetchAll((products) => {
+        res.status(200).json({ products: products });
+    });
+};
 
 exports.getProduct = (req, res) => {
     const params = req.params;
-    Product.findById( params.productId,products => {
-        res.status(200).json({
-            product: products
-        })
-    })
-}
+    Product.findById(params.productId, (product) => {
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        res.status(200).json({ product: product });
+    });
+};
 
 exports.deleteProduct = (req, res) => {
     const params = req.params;
-    Product.deleteById(params.productId);
-    res.status(200).json({
-        message: 'Product Deleted'
-    })
-}
+    Product.deleteById(params.productId, (err) => {
+        if (err) {
+            return res.status(500).json({ message: 'Failed to delete product' });
+        }
+        res.status(200).json({ message: 'Product deleted' });
+    });
+};
 
 exports.updateProduct = (req, res) => {
     const params = req.params;
     const body = req.body;
-    Product.updateProduct( params.productId, body);
-    res.status(200).json({
-        message: 'Product Updated'
-    })
-}
+    Product.updateProduct(params.productId, body, (err) => {
+        if (err) {
+            return res.status(500).json({ message: 'Failed to update product' });
+        }
+        res.status(200).json({ message: 'Product updated' });
+    });
+};
