f0206b44e75f747aa8c52f66e1f5b26b6dd8428c
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-06-21 15:49:20 +0700

    Update MySQLDB.js

--- models/database/MySQLDB.js
+++ models/database/MySQLDB.js
@@ -10,36 +10,62 @@ const pool = mysql.createPool({
 class MySQLDB {
     save(product, cb) {
         const sql = 'INSERT INTO products (name, price, screen, backCamera, frontCamera, img, desc, type) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-        pool.execute(sql, [product.name, product.price, product.screen, product.backCamera, product.frontCamera, product.img, product.desc, product.type], (err, results) => {
-            cb(err);
+        pool.execute(sql, [
+            product.name, product.price, product.screen,
+            product.backCamera, product.frontCamera,
+            product.img, product.desc, product.type
+        ], (err, results) => {
+            if (err) {
+                return cb(err);
+            }
+            cb(null, { insertedId: results.insertId });
         });
     }
 
     fetchAll(cb) {
         const sql = 'SELECT * FROM products';
         pool.execute(sql, [], (err, results) => {
-            cb(results);
+            if (err) {
+                return cb(err);
+            }
+            cb(null, results);
         });
     }
 
     findById(id, cb) {
         const sql = 'SELECT * FROM products WHERE id = ?';
         pool.execute(sql, [id], (err, results) => {
-            cb(results[0]);
+            if (err) {
+                return cb(err);
+            }
+            if (results.length === 0) {
+                return cb({ status: 404, message: 'Product not found', success: false });
+            }
+            cb(null, results[0]);
         });
     }
 
     deleteById(id, cb) {
         const sql = 'DELETE FROM products WHERE id = ?';
         pool.execute(sql, [id], (err, results) => {
-            cb(err);
+            if (err) {
+                return cb(err);
+            }
+            cb(null, { status: 200, message: 'Product deleted', success: true });
         });
     }
 
     updateProduct(id, updatedProduct, cb) {
         const sql = 'UPDATE products SET name = ?, price = ?, screen = ?, backCamera = ?, frontCamera = ?, img = ?, desc = ?, type = ? WHERE id = ?';
-        pool.execute(sql, [updatedProduct.name, updatedProduct.price, updatedProduct.screen, updatedProduct.backCamera, updatedProduct.frontCamera, updatedProduct.img, updatedProduct.desc, updatedProduct.type, id], (err, results) => {
-            cb(err);
+        pool.execute(sql, [
+            updatedProduct.name, updatedProduct.price, updatedProduct.screen,
+            updatedProduct.backCamera, updatedProduct.frontCamera,
+            updatedProduct.img, updatedProduct.desc, updatedProduct.type, id
+        ], (err, results) => {
+            if (err) {
+                return cb(err);
+            }
+            cb(null, { status: 200, message: 'Product updated', success: true });
         });
     }
 }
